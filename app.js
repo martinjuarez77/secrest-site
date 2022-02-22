d32138112413b3c005a70cb02092796b6420b45c
Martin Juarez Acheritobehere <martinjuarez77rgmail.com>
2022-02-22 06:54:33 +0100

    first commit with pass encryption

---
+++ app.js
@@ -0,0 +1,88 @@
+//jshint esversion:6
+require('dotenv').config();
+//console.log(process.env);
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
+
+const userSchema = new mongoose.Schema( {
+  email: String,
+  password: String
+});
+
+const secret = process.env.DB_KEY;
+userSchema.plugin(encrypt, {secret: secret, encryptedFields: ["password"]});
+
+
+const User = mongoose.model("User", userSchema);
+
+
+
+app.get("/", function(request, response){
+  response.render("home");
+});
+
+app.get("/login", function(request, response){
+  response.render("login");
+});
+
+app.get("/register", function(request, response){
+  response.render("register");
+});
+
+app.post("/register", function(request, response){
+  const newUser = new User ({
+    email: request.body.username,
+    password: request.body.password
+  });
+
+  newUser.save(function(error){
+    if (error){
+      console.log ("Error al crear usuario");
+    } else {
+      response.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(request, response){
+
+    const username = request.body.username;
+    const password =  request.body.password;
+
+    User.findOne({email: username}, function (error, foundUser){
+      if (error){
+        console.log("error- >" + error);
+      } else {
+        if (foundUser){
+          console.log("foundUser.password -> " + foundUser.password);
+          console.log("password -> " + password);
+          if (foundUser.password === password){
+            response.render("secrets")
+          } else {
+            response.render("register");
+          }
+        } else {
+          response.render("home");
+        }
+      }
+    });
+});
+
+
+app.listen(8086, function(){
+  console.log("Running in port 8086");
+})
